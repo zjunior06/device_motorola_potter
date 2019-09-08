c4dfe221939491e51a6cfebabc2b2df1c0e3e47d
Erfan Abdi <erfangplus@gmail.com>
2019-09-08 19:04:43 +0530

    potter: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- du.mk
+++ du.mk
@@ -23,6 +23,9 @@ $(call inherit-product, vendor/du/config/common_full_phone.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # TWRP theme
 TW_THEME := portrait_hdpi
 
