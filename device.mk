c76b916075dbe4ef4228161a25e3985e82136d74
William Bellavnce <flintman@flintmancomputers.com>
2019-11-12 10:43:38 -0500

    Snap:  Switch to snap.

--- device.mk
+++ device.mk
@@ -106,8 +106,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    Camera2 \
-    libgui_vendor \
+    Snap \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
