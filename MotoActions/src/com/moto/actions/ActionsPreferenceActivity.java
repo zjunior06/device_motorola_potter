855823f4b1c63fee2dd9d0406ccab83ac3ac2440
William Bellavnce <flintman@flintmancomputers.com>
2019-11-27 08:11:56 -0500

    Revert "Refactor gestures activity to match other devices"
    This reverts commit d43fd3753a37011e74323a519ca27f293d408b14.

--- MotoActions/src/com/moto/actions/ActionsPreferenceActivity.java
+++ MotoActions/src/com/moto/actions/ActionsPreferenceActivity.java
@@ -20,7 +20,7 @@ import android.os.Bundle;
 
 import android.preference.PreferenceActivity;
 
-public class TouchscreenGestureSettings extends PreferenceActivity {
+public class ActionsPreferenceActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
