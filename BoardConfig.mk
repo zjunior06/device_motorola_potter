6bf754514fa93e6381286efba05bb9faa845271c
kubersharma001 <kubersharma001@gmail.com>
2020-01-31 14:40:32 +0530

    potter: Drop WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY flag
     * To DEXPREOPT all ^.^
    
    Change-Id: I01eab78cbbfebcd9def568034048dde72ea61c2d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,7 +59,6 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_OTA_ASSERT_DEVICE := potter,potter_retail
 
 # Enable dexpreopt to speed boot time
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 WITH_DEXPREOPT := true
 #PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 TARGET_FLATTEN_APEX := true
