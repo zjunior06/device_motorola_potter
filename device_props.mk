2b4cabe7e8c3df6924facee41e1a9cd5a88d5805
Aaron Kling <webgeek1234@gmail.com>
2020-10-10 10:44:57 -0500

    Enable userspace lmkd
    Change-Id: I80d8786d5f93d5ba101c6bc0e652f6a3f14d1e34

--- device_props.mk
+++ device_props.mk
@@ -189,6 +189,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.voice_on_lte=1 \
     persist.vendor.radio.calls.on.ims=1 \
 
+# LMKd
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.aac_51_output_enabled=true \
