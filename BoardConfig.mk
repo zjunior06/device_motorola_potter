df8dc70357cf3c7df7d2b1b0080546d008f7dc21
William Bellavance <flintman@flintmancomputers.com>
2021-05-24 06:24:31 -0400

    overlay: move sdk to new path
    Also move over to overlay-ev
    
    Change-Id: I7cc4c9d027c810952dd9626a3b8f15f927a63c16

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,8 @@ DEVICE_PATH := device/moto/potter
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay \
+      $(DEVICE_PATH)/overlay-ev
 
 BOARD_VENDOR := motorola-qcom
 
