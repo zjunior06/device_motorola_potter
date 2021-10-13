8f4d9f1220e0f201e9aff2769035b605c0fae8c8
zjrdroid <priv.zjr@gmail.com>
2021-10-13 15:07:51 +0000

    BOARD_PLAT > SYSTEM_EXT and go permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,7 +74,7 @@ TARGET_RECOVERY_DEVICE_MODULES := //$(DEVICE_PATH):libinit_potter
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M loop.max_part=16
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_PAGESIZE := 2048
@@ -227,7 +227,8 @@ TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SEPOLICY_IGNORE_NEVERALLOWS := true
 
 # Sensor
 USE_SENSOR_MULTI_HAL := true
