f083dc305780734f78d7ae3963672ed4780240c9
Akash98Sky <Akash98Sky@users.noreply.github.com>
2021-10-18 14:03:14 +0000

    potter: Build mock power stats hal
    fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.

--- device.mk
+++ device.mk
@@ -314,7 +314,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
