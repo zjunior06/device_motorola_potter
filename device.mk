df019c098ea7e70cb0ee561e0662bb15ed61ac05
Edwin Wong <>
2021-10-27 06:40:05 -0400

    potter: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I966bc89e18d307a4c6a356e7647b272b24e86de7

--- device.mk
+++ device.mk
@@ -182,7 +182,7 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
